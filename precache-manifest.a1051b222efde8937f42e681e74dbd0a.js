f88ddfbf4167de4e7f58a99337b113a6936e1dfa
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-02 00:39:59 -0300

    Updates

---
+++ precache-manifest.a1051b222efde8937f42e681e74dbd0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619d2f6d0ec8a5f236acfd427c3755fc",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "dfc2daaa0d274426baad",
+    "url": "/frontend-online-store/static/css/main.b0bf2529.chunk.css"
+  },
+  {
+    "revision": "2100ebd9415318fd0ed3",
+    "url": "/frontend-online-store/static/js/2.0463a62b.chunk.js"
+  },
+  {
+    "revision": "aab1e26f46dc635b5f5e5a548f4847ab",
+    "url": "/frontend-online-store/static/js/2.0463a62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc2daaa0d274426baad",
+    "url": "/frontend-online-store/static/js/main.1cb1201a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/frontend-online-store/static/js/main.1cb1201a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  }
+]);
